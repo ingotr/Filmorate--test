3be5f8d92803e2de3e4ae658f384b1c834a6c15f
Aleksandr Markovkin <markovkin88@gmail.com>
2023-05-17 19:40:58 +0300

    2. Добавить модели данных

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Data
+public class Film {
+    private final int id;
+    private final String name;
+    private final String description;
+    private final LocalDateTime releaseDate;
+    private final Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class User {
+    private final int id;
+    private final String email;
+    private final String login;
+    private final String name;
+    private final LocalDateTime birthday;
+}
