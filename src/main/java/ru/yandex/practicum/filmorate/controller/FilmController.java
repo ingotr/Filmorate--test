1e9876e3f681393934b12e07502b590db00685c9
Aleksandr Markovkin <markovkin88@gmail.com>
2023-05-17 21:12:57 +0300

    8. Добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -10,31 +11,39 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private int idCounter = 1;
     private final Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping
     public Map<Integer, Film> getAllFilms() {
+        log.debug("Получен запрос GET /films.");
+        log.debug("Текущее количество фильмов: {}", films.size());
         return films;
     }
 
     @PostMapping
     public Film createFilm(@RequestBody Film film) {
+        log.debug("Получен запрос POST /films.");
         isFilmTitleValid(film);
+        log.debug("Добавлен новый фильм с Id: " + film.getId());
         return film;
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
+        log.debug("Получен запрос PUT /films.");
         try {
             if (film.getId() != 0) {
                 films.replace(film.getId(), film);
             }
 
         } catch (RuntimeException e) {
+            log.warn("Не указан Id фильма");
             throw new ValidationException("Не указан Id фильма. Укажите и попробуйте снова");
         }
+        log.debug("Обновлен фильм с Id: " + film.getId());
         return film;
     }
 
@@ -44,6 +53,7 @@ public class FilmController {
                 isFilmDescriptionLengthValid(film);
             }
         } catch (RuntimeException e) {
+            log.warn("Не указано название фильма");
             throw new ValidationException("Не указано название фильма. Укажите и попробуйте снова");
         }
     }
@@ -54,6 +64,7 @@ public class FilmController {
                 isFilmReleaseDateValid(film);
             }
         } catch (RuntimeException e) {
+            log.warn("Длина описания больше 200 символов");
             throw new ValidationException("Максимальная длина описания - 200 символов.");
         }
     }
@@ -64,6 +75,7 @@ public class FilmController {
                 isFilmDurationPositive(film);
             }
         } catch (RuntimeException e) {
+            log.warn("Дата релиза раньше 28 декабря 1895 года");
             throw new ValidationException("Дата релиза — не может быть раньше 28 декабря 1895 года");
         }
     }
@@ -74,6 +86,7 @@ public class FilmController {
                 films.put(getIdCounter(), film);
             }
         } catch (RuntimeException e) {
+            log.warn("Продолжительность фильма отрицательная или равна 0");
             throw new ValidationException("Продолжительность фильма должна быть положительной");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -10,32 +11,39 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private int idCounter = 1;
     private final Map<Integer, User> users = new HashMap<>();
 
     @GetMapping
     public Map<Integer, User> getAllUsers() {
+        log.debug("Получен запрос GET /users.");
+        log.debug("Текущее количество пользователей: {}", users.size());
         return users;
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
-        users.put(user.getId(), user);
+        log.debug("Получен запрос POST /users.");
         isUserEmailValid(user);
+        log.debug("Добавлен новый пользователь с Id: " + user.getId());
         return user;
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
+        log.debug("Получен запрос PUT /users.");
         try {
             if (user.getId() != 0) {
                 users.replace(user.getId(), user);
             }
 
         } catch (RuntimeException e) {
+            log.warn("Не указан Id пользователя");
             throw new ValidationException("Не указан Id пользователя. Укажите и попробуйте снова");
         }
+        log.debug("Обновлен пользователь с Id: " + user.getId());
         return user;
     }
 
@@ -45,6 +53,7 @@ public class UserController {
                 isUserLoginValid(user);
             }
         } catch (RuntimeException e) {
+            log.warn("Не указана электронная почта либо неверный формат почты");
             throw new ValidationException("Не указана электронная почта либо неверный формат почты: должна содержать @. " +
                     "Укажите и попробуйте снова");
         }
@@ -54,6 +63,7 @@ public class UserController {
         try {
             isUserBirthdayValid(user);
         } catch (RuntimeException e) {
+            log.warn("Пусто логин");
             throw new ValidationException("Логин не может быть пустым");
         }
     }
@@ -63,6 +73,7 @@ public class UserController {
             try {
                 isUserNameBlank(user);
             } catch (RuntimeException e) {
+                log.warn("Дата рождения не может быть в будущем");
                 throw new ValidationException("Дата рождения не может быть в будущем");
             }
         }
